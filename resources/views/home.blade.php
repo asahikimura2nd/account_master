1c4db346d1b4e69b1d2a3d82eb173600c1d06d83
kimura2nd <asahikimura.ak@gmail.com>
2022-08-17 14:12:23 +0900

    レイアウト作成中

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,33 +5,43 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ホーム</title>
-    <script src="{{ asset('/js/app.js') }}" defer></script>
-    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
-    {{-- <link rel="stylesheet" href="{{ asset('/css/signin.css') }}"> --}}
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> --}}
+    <link rel="stylesheet" href="{{ asset('css/reset.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
 </head>
 <body class="text-center">
-    {{-- ログイン成功 --}}
-    {{-- @if(session('login_success'))
-      <div>{{session('login_success')}}</div>
-    @endif
-    プロフィール --}}
-  {{-- userモデルのデータを表示する --}}
-    {{-- <li>名前：{{Auth::user()->name}}</li> --}}
+
 
   <header>
-    <div>管理者：{{Auth::user()->email}}</div>
-    <form action="{{route('logout')}}" method="POST">
-      @csrf
-      <input type="submit" value="ログアウト">
-    </form>
+    <div class="admin">管理者：{{Auth::user()->email}}
+      <form action="{{route('logout')}}" method="POST" class="logoutForm">
+        @csrf
+        <button class="logoutButton" type="submit">ログアウト</button>
+      </form>
+    </div>
+ 
   </header>  
     
   <main>
-    <div>HOME</div>
-    <div>Logout</div>
-    
-    <div class="container">    
+    <div class="sideBar active" id="sideBar">
+      <div class="items ">
+        <div class="hambarger"><img src="{{asset('images/menu50.png')}}" alt="hambarger" class="menuIcon"></div>
+        <div class="home">
+          <div><img src="{{asset('images/home50.png')}}" alt="home" class="homeIcon"></div>
+          <div>ホーム</div>
+        </div>
+        <div class="members">
+          <div><img src="{{asset('images/component48.png')}}" alt="members" class="membersIcon"></div>
+          <div>会員登録</div>
+        </div>
+      </div>
+    </div>
+   
+ 
+    <div class="mainContainer">    
+      <div>HOME</div>
       <div class="card1">
         <div class="card2">
           <div>会員登録</div>
@@ -46,19 +56,7 @@
     </div>  
   </main>
 
-  <div class="sideBar active" id="sideBar">
-    <div class="items ">
-      <div class="hambarger"><img src="" alt="hambarger"></div>
-      <div class="home">
-        <div><img src="asset{{'images/home50.png'}}" alt="home"></div>
-        <div>ホーム</div>
-      </div>
-      <div class="members">
-        <div><img src="asset{{'images/component48.png'}}" alt="members"></div>
-        <div>会員登録</div>
-      </div>
-    </div>
-  </div>
+
 	
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 </body>
